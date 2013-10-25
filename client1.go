988311546a2468d99133952649d993afe793909e
work <work@ijibu.com>
2013-10-25 17:41:07 +0800

    修改程序运行方式，加入log 	并发情况下写log思路要变啊，同时写一个文件存在锁的情况啊 Signed-off-by: work <work@ijibu.com>

--- client1.go
+++ client1.go
@@ -12,7 +12,6 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
-	"time"
 )
 
 const (
@@ -22,7 +21,8 @@ const (
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU()) //设置cpu的核的数量，从而实现高并发
 	logfile, _ := os.OpenFile("./test.log", os.O_RDWR|os.O_CREATE, 0)
-	c := make(chan bool)
+	logger := log.New(logfile, "\r\n", log.Ldate|log.Ltime|log.Llongfile)
+	c := make(chan int, 1920)
 	fh, ferr := os.Open("./shen.ini")
 	if ferr != nil {
 		return
@@ -33,21 +33,20 @@ func main() {
 	for i := 0; i < 192; i++ { //加入goroutine缓冲，4个执行完了再执行下面的4个
 		for k := 0; k < 10; k++ {
 			input, _ := inputread.ReadString('\n')
-			go getShangTickerTables(logfile, c, i, strings.TrimSpace(input))
+			go func(logger *log.Logger, logfile *os.File, i int, input string) {
+				getShangTickerTables(logger, logfile, i, input)
+				c <- 0
+			}(logger, logfile, i, strings.TrimSpace(input))
 		}
-		<-c
 	}
-
-	for {
-		time.Sleep(60000 * time.Second)
-		fmt.Println("main ok")
+	defer logfile.Close()
+	for j := 0; j < 1920; j++ {
+		<-c
 	}
 }
 
-func getShangTickerTables(logfile *os.File, c chan bool, n int, code string) {
-	logger := log.New(logfile, "\r\n", log.Ldate|log.Ltime|log.Llongfile)
-	defer logfile.Close()
-	flg := make(chan bool, 1)
+func getShangTickerTables(logger *log.Logger, logfile *os.File, n int, code string) {
+	//并发写文件必须要有锁啊，怎么还是串行程序的思维啊。
 	fileName := "./data/sz/" + code + ".csv"
 	f, err := os.OpenFile(fileName, os.O_CREATE, 0666) //其实这里的 O_RDWR应该是 O_RDWR|O_CREATE，也就是文件不存在的情况下就建一个空文件，但是因为windows下还有BUG，如果使用这个O_CREATE，就会直接清空文件，所以这里就不用了这个标志，你自己事先建立好文件。
 	if err != nil {
@@ -56,58 +55,33 @@ func getShangTickerTables(logfile *os.File, c chan bool, n int, code string) {
 
 	defer f.Close()
 
-	go func(logfile *os.File, f *os.File, n int, code string) {
-		urls := "http://table.finance.yahoo.com/table.csv?s=" + code + ".ss"
-		var req http.Request
-		req.Method = "GET"
-		req.Close = true
-		req.URL, err = url.Parse(urls)
-		if err != nil {
-			panic(err)
-		}
-
-		header := http.Header{}
-		header.Set("User-Agent", UA)
-		req.Header = header
-		resp, err := http.DefaultClient.Do(&req)
-		if err == nil {
-			if resp.StatusCode == 200 {
-				logger.Println(code + ":sucess" + strconv.Itoa(resp.StatusCode))
-				io.Copy(f, resp.Body)
-			} else {
-				logger.Println(code + ":http get StatusCode" + strconv.Itoa(resp.StatusCode))
-				f.WriteString("http get StatusCode" + strconv.Itoa(resp.StatusCode))
-			}
-			defer resp.Body.Close()
-		} else {
-			logger.Println(code + ":http get error" + code)
-			f.WriteString("http get error")
-		}
-		flg <- true
-	}(logfile, f, n, code)
-
-	//加入超时设置
-	timeout := make(chan bool, 1)
-	go func() {
-		time.Sleep(500 * time.Second)
-		timeout <- true
-	}()
-
-	select {
-	case <-flg:
-		//正常退出
-		if n%10 == 0 {
-			c <- true
-		}
-		return
-	case <-timeout:
-		//超时退出
-		if n%10 == 0 {
-			c <- true
-		}
-		logger.Println(code + ":http get timeout" + code)
-		f.WriteString("timeouttimeouttimeouttimeouttimeouttimeouttimeouttimeouttimeouttimeouttimeouttimeout")
-		return
+	urls := "http://table.finance.yahoo.com/table.csv?s=" + code + ".ss"
+	var req http.Request
+	req.Method = "GET"
+	req.Close = true
+	req.URL, err = url.Parse(urls)
+	if err != nil {
+		panic(err)
 	}
 
+	header := http.Header{}
+	header.Set("User-Agent", UA)
+	req.Header = header
+	resp, err := http.DefaultClient.Do(&req)
+	if err == nil {
+		if resp.StatusCode == 200 {
+			logger.Println(logfile, code+":sucess"+strconv.Itoa(resp.StatusCode))
+			fmt.Println(code + ":sucess" + strconv.Itoa(resp.StatusCode))
+			io.Copy(f, resp.Body)
+		} else {
+			logger.Println(logfile, code+":http get StatusCode"+strconv.Itoa(resp.StatusCode))
+			fmt.Println(code + ":http get StatusCode" + strconv.Itoa(resp.StatusCode))
+			f.WriteString("http get StatusCode" + strconv.Itoa(resp.StatusCode))
+		}
+		defer resp.Body.Close()
+	} else {
+		logger.Println(logfile, code+":http get error"+code)
+		fmt.Println(code + ":http get error" + code)
+		f.WriteString("http get error")
+	}
 }
